a15539665360c97ae4433dcd01649d4577eddcb6
pratyush singh <pratyush.singh@globant.com>
2019-06-18 11:34:58 +0530

    new program

---
+++ src/competitive_question/Pallandrome_String.java
@@ -0,0 +1,13 @@
+package competitive_question;
+
+import java.util.Scanner;
+
+public class Pallandrome_String {
+    public static void main(String[] args) {
+        String s = new Scanner(System.in).next();
+        System.out.println(s);
+        for (int i = 0; i <= s.length(); i++) {
+
+        }
+    }
+}
