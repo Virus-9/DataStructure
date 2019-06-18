221f485a8c5de08ce8ba796cf7325819e43e9234
pratyush singh <pratyush.singh@globant.com>
2019-06-18 11:34:23 +0530

    new program

--- src/datastructure/Junk.java
+++ src/datastructure/Junk.java
@@ -1,12 +1,27 @@
 package datastructure;
 
-public class Junk {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
+    /**
+     * Java program to calculate and print Fibonacci number using both recursion
+     * and Iteration.
+     * Fibonacci number is sum of previous two Fibonacci numbers fn= fn-1+ fn-2
+     * first 10 Fibonacci numbers are 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+     *
+     * @author Javin
+     */
+    public class Junk {
+            static int fib(int n)
+            {
+                if (n <= 1)
+                    return n;
+                return fib(n-1) + fib(n-2);
+            }
 
+            public static void main (String args[])
+            {
+                int n = 3;
 
-        System.out.print("");
-        System.out.println(1);
-    }
-
-}
+                System.out.println(fib(n));
+            }
+        }
